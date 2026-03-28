303153f7fb30864b88859424a045ac9dc117e6fa
Naveen32716e <naveenkumarreddykarri1@gmail.com>
2026-03-28 20:24:10 +0530

    SmartQ project1

--- server/server.js
+++ server/server.js
@@ -40,10 +40,11 @@ const startServer = async () => {
   try {
     await connectDB(); // ✅ WAIT FOR DB
 
-    server.listen(5000, () => {
-      console.log("MongoDB Connected ✅");
-      console.log("Server + Socket running on port 5000 🚀");
-    });
+    const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
   } catch (err) {
     console.error("DB connection failed ❌", err);
